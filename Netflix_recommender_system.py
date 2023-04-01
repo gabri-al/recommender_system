62253c8a0e6e04c813a302f143b2a419131f63a3
Gabriele Albini <57110246+gabri-al@users.noreply.github.com>
2023-04-01 14:54:46 +0200

    Update Netflix_recommender_system.py

--- Netflix_recommender_system.py
+++ Netflix_recommender_system.py
@@ -2,8 +2,10 @@ import pandas as pd
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-from surprise import Reader, Dataset, SVD
+import time
+from surprise import Reader, Dataset, SVD, dump
 from surprise.model_selection import cross_validate
+from pymongo import MongoClient
 
 ############################################################
 # Data loading
@@ -186,3 +188,39 @@ pred_ratings = pd.DataFrame(data=pred_data, columns=['Movie_Id', 'Cust_Id', 'Pre
 pred_ratings = pd.merge(left=pred_ratings.set_index(['Movie_Id']), right=df_mov_titles['Name'],
                         how='left', left_index=True, right_index=True)
 print(pred_ratings.sort_values(by=['Predicted_Rating','Name'], ascending=[False,True]).head(20))
+
+############################################################
+# Predictions for all users and movies
+############################################################
+## Create predictions for all movies and users
+st = time.time()
+preds_ = []
+movieids = np.sort(df_ratings_XS['Movie_Id'].unique())
+userids = np.sort(df_ratings_XS['Cust_Id'].unique())
+
+for mo in movieids:
+    preds_.append([svd.predict(cc,mo).est for cc in userids])
+
+elapsed_ = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time()-st))
+print("Predictions completed in: {}".format(elapsed_))
+
+## Transform predictions into dataframe only for missing ratings
+st = time.time()
+
+# Identify movies rated already
+df_ratings_XS['K'] = df_ratings_XS['Cust_Id'].astype(str)+'_'+df_ratings_XS['Movie_Id'].astype(str)
+Keys_rated = df_ratings_XS['K'].unique()
+Keys_rated = set(Keys_rated)
+
+# Identify missing ratings
+preds_df_data = []
+for i in range(len(preds_)):
+    for j in range(len(preds_[i])):
+        if str(userids[j])+'_'+str(movieids[i]) not in Keys_rated:
+            preds_df_data.append([movieids[i], userids[j], preds_[i][j]])
+preds_df = pd.DataFrame(data=preds_df_data, columns=['Movie_Id', 'Cust_Id', 'Predicted_Rating'])
+
+elapsed_ = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time()-st))
+print("Dataframe creation completed in: {}".format(elapsed_))
+
+print(preds_df.head(3))
